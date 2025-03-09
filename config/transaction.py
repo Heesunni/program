353c45c79537449b7897c825ff6aadcabf49f82a
Heesunni <kikiplus1@naver.com>
2025-03-09 21:43:57 +0900

    기타

--- config/transaction.py
+++ config/transaction.py
@@ -1,24 +1,23 @@
 from functools import wraps
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
-from config.database import get_db  # ✅ DB 세션을 가져오는 함수
+from config.database import get_db
 
 def transactional(func):
-    """Spring의 @Transactional과 유사한 기능을 제공하는 데코레이터"""
     @wraps(func)
     def wrapper(self, *args, **kwargs):
-        session: Session = next(get_db())  # ✅ Generator에서 실제 Session 객체 가져오기
+        session: Session = next(get_db())
         try:
-            with session.begin():  # ✅ 트랜잭션 시작
-                kwargs["session"] = session  # ✅ session을 kwargs에 추가
-                result = func(self, *args, **kwargs)  # ✅ session을 인자로 전달하지 않고 kwargs로 넘김
-                session.commit()  # ✅ 트랜잭션 커밋
+            with session.begin():
+                kwargs["session"] = session
+                result = func(self, *args, **kwargs)
+                session.commit()
                 return result
         except HTTPException as e:
-            session.rollback()  # ❌ 예외 발생 시 롤백
+            session.rollback()
             raise e
         except Exception as e:
-            session.rollback()  # ❌ 기타 예외 발생 시 롤백
+            session.rollback()
             raise HTTPException(500, str(e))
 
     return wrapper
